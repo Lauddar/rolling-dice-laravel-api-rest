20ebcc3b56a2a29e6df0bcdad62e6e8873f11f31
lgrodriguez <lauragarciarod@gmail.com>
2023-02-23 19:04:06 +0100

    modify users table

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'nickname',
         'email',
         'password',
     ];
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getNicknameAttribute($value)
+    {
+        return $value ?: 'Anonymous';
+    }
 }
