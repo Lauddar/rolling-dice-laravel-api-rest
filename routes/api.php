9c55984755a476cbcbc56a9aadd1e487171e3c8b
lgrodriguez <lauragarciarod@gmail.com>
2023-02-23 23:32:47 +0100

    add routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\UserController;
+use App\Http\Controllers\api\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/login', [LoginController::class, 'login']);
+
+Route::middleware('auth:api')->prefix('players')->group(function () {
+    Route::get('/', [UserController::class, 'all']);
 });
