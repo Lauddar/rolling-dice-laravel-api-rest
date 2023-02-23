51ef74bb3870c8d258a140bbb473f6171adc4986
lgrodriguez <lauragarciarod@gmail.com>
2023-02-23 23:32:25 +0100

    login

---
+++ app/Http/Controllers/api/LoginController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Passport\HasApiTokens;
+
+class LoginController extends Controller
+{
+    use HasApiTokens;
+
+    public function login(Request $request) {
+        $login = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        $credentials = $request->only('email','password');
+
+        if (!Auth::attempt($credentials)) {
+            return response(['message' => 'Invalid login credentials.'], 401);
+        }
+
+        $accesToken = Auth::user()->createToken('authToken')->accessToken;
+
+        return response(['user' => Auth::user(), 'acces_token' => $accesToken]);
+    }
+}
+
