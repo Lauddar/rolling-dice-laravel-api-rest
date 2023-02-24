345dbb755df69aad4cb048d28a4be9590c77bd79
lgrodriguez <lauragarciarod@gmail.com>
2023-02-24 16:14:27 +0100

    routes

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\UserController;
 use App\Http\Controllers\api\LoginController;
+use App\Http\Controllers\api\GameController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,4 +23,5 @@ Route::middleware('auth:api')->prefix('players')->group(function () {
     Route::get('/', [UserController::class, 'index']);
     Route::post('/', [UserController::class, 'store']);
     Route::post('/{user}', [UserController::class, 'update']);
+    Route::post('/{user}/games', [GameController::class, 'play']);
 });
