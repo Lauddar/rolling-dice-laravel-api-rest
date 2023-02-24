328c3d4e6b8e9bf0a351cd2801d3db18d22d90fe
lgrodriguez <lauragarciarod@gmail.com>
2023-02-24 16:14:13 +0100

    model

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get the games for the user.
+     */
+    public function games()
+    {
+        return $this->hasMany(Game::class);
+    }
 }
