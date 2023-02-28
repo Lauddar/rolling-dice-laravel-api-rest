30dea09bca78c91e80f2851db32a464d25f5957f
lgrodriguez <lauragarciarod@gmail.com>
2023-02-28 11:54:43 +0100

    migrations for roles

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create(
+            [
+                'nickname' => 'Test Admin User',
+                'email' => 'admin@test.com',
+                'password' => '$2y$10$/4OC8ZMqeCTMzb9RhRca1evVw2YDPP1wuIGWP58NqADCyp.0G.U.W',
+                'succes_rate' => 0
+            ],
+        )->assignRole(['Admin']);
+
+        User::create(
+            [
+                'nickname' => 'Test Player User',
+                'email' => 'player@test.com',
+                'password' => '$2y$10$/4OC8ZMqeCTMzb9RhRca1evVw2YDPP1wuIGWP58NqADCyp.0G.U.W',
+                'succes_rate' => 0
+            ]
+        )->assignRole(['Player']);
+    }
+}
