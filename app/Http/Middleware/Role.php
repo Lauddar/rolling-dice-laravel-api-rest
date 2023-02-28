a17c0e33bd6aa0cb14bc4850508ac70e2da4b45a
lgrodriguez <lauragarciarod@gmail.com>
2023-02-28 11:54:56 +0100

    add roles restrinction to routes

---
+++ app/Http/Middleware/Role.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class Role
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle($request, Closure $next, $role)
+    {
+        if (! $request->user() || ! $request->user()->hasRole($role)) {
+            abort(403, 'Unauthorized action.');
+        }
+    
+        return $next($request);
+    }
+}
