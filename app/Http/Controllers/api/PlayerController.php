37510404a91b6d491497a238d3b313b6db5a5a27
lgrodriguez <lauragarciarod@gmail.com>
2023-03-01 17:30:57 +0100

    fix index player

--- app/Http/Controllers/api/PlayerController.php
+++ app/Http/Controllers/api/PlayerController.php
@@ -4,10 +4,16 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 
+use App\Models\Player;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class PlayerController extends Controller
 {
-    
+    public function index()
+    {
+        $players = Player::with('user')->get();    
+
+        return response(['players' => $players]);
+    }
 }
