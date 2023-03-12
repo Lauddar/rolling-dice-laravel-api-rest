ee6330cfe14b9c6fda76cf9a7ef6eab98bb50fcf
Lauddar <lauragarciarod@gmail.com>
2023-03-12 11:48:09 +0100

    config cors headers

--- routes/api.php
+++ routes/api.php
@@ -19,21 +19,24 @@ use App\Http\Controllers\api\RankController;
 */
 
 Route::post('/login', [LoginController::class, 'login'])->name('login');
-Route::post('/players', [UserController::class, 'store'])->name('register');
+
+Route::middleware(['cors'])->group(function () {
+    Route::post('/players', [UserController::class, 'store'])->name('register');
+});
+
+//Admin permission routes
+Route::middleware(['auth:api', 'cors', 'role:Admin'])->prefix('players')->group(function () {
+    Route::get('/', [UserController::class, 'index'])->name('players.index');
+    Route::get('/ranking', [RankController::class, 'rank'])->name('admin.ranking');
+    Route::get('/ranking/loser', [RankController::class, 'loser'])->name('admin.ranking.loser');
+    Route::get('/ranking/winner', [RankController::class, 'winner'])->name('admin.ranking.winner');
+});
 
 //Player permission routes
-Route::middleware('auth:api')->prefix('players')->group(function () {
+Route::middleware(['auth:api', 'cors'])->prefix('players')->group(function () {
     Route::put('/{user}', [UserController::class, 'update']);
     Route::get('/{user}', [UserController::class, 'getUser']);
     Route::post('/{user}/games', [GameController::class, 'play']);
     Route::delete('/{user}/games', [GameController::class, 'delete']);
     Route::get('/{user}/games', [GameController::class, 'index']);
-});
-
-//Admin permission routes
-Route::middleware(['auth:api', 'role:Admin'])->prefix('players')->group(function () {
-    Route::get('/', [UserController::class, 'index'])->name('players.index');
-    Route::get('/ranking', [RankController::class, 'rank'])->name('admin.ranking');
-    Route::get('/ranking/loser', [RankController::class, 'loser'])->name('admin.ranking.loser');
-    Route::get('/ranking/winner', [RankController::class, 'winner'])->name('admin.ranking.winner');
 });
